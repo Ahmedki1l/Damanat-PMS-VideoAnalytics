91bb4b03acccf100134b244c84b261162ac19682
Mohamed Henaish <mohamed,henaish2000@gmail.com>
2026-03-28 14:50:20 +0200

    change from postgrace to mysql

--- src/model/slots_status.py
+++ src/model/slots_status.py
@@ -7,9 +7,9 @@ from datetime import datetime
 class SlotStatus(Base):
     __tablename__ = "slot_status"
     id = Column(Integer, primary_key=True, index=True , autoincrement=True)
-    slot_id = Column(String, ForeignKey("parking_slots.slot_id"),nullable=False)
-    plate_number = Column(String, index=True)
-    status = Column(String)
+    slot_id = Column(String(50), ForeignKey("parking_slots.slot_id"),nullable=False)
+    plate_number = Column(String(20), index=True)
+    status = Column(String(20))
     time = Column(DateTime, default=datetime.utcnow)
 
     slot = relationship(
